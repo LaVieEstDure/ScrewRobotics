bb16ee42cd9a49845653b19f8f98ae3c6d1285a9
Raghav Mishra <mishraraghav24@gmail.com>
2021-09-11 16:56:21 +1000

    Initial rotation API

--- src/representations.py
+++ src/representations.py
@@ -1,10 +1,17 @@
 import numpy as np 
 
-class EulerVec:
-    def __init__(x, y, z):
+class Rotation:
+    def __init__(self, x, y, z):
         self.euler_vec = np.array([x, y, z])
     
-    def to_so3():
+    @property
+    def so3(self):
         return np.array([[0, -self.euler_vec[2], self.euler_vec[1]], 
                         [self.euler_vec[2], 0, -self.euler_vec[0]], 
                         [-self.euler_vec[1], self.euler_vec[0], 0]])
+    @property
+    def SO3(self):
+        theta = np.abs(self.euler_vec)
+        norm_vector = self.euler_vec/theta
+        return np.eye(3) - np.sin(theta)*self.so3 + (1-np.cos(theta))*(self.so3 @ self.so3)
+    
