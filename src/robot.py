6fedfec83595c8a2496bf501c145105f3b568ad9
Raghav Mishra <mishraraghav24@gmail.com>
2021-10-21 16:02:49 +1000

    Change files

--- src/robot.py
+++ src/robot.py
@@ -5,20 +5,24 @@ from representations import Transformation
 import numpy as np
 from math import *
 
+
 class Frame(Enum):
     SPACE_FRAME = 0
     BODY_FRAME = 1
 
+
 class Robot:
-    def __init__(self, M_list, screw_list, frame):
+    def __init__(self, M_list, screw_list, frame, ik=None):
         assert len(M_list) == len(screw_list) + 2
         self.n = len(screw_list)
         self.M_list = M_list
         self.screw_list = [*map(lambda x: Transformation(*x), screw_list)]
         self.frame = frame
+        self.ik = ik
 
     def forward_kinematics(self, theta_list):
-        non_norm = [theta * self.screw_list[i] for (i, theta) in enumerate(theta_list)]
+        non_norm = [theta * self.screw_list[i]
+                    for (i, theta) in enumerate(theta_list)]
         T = [None] * (self.n + 2)
         T[0] = np.eye(4)
         for i in range(1, self.n + 2):
@@ -32,34 +36,38 @@ class Robot:
             else:
                 raise ValueError("Wrong frame specified!")
         return T
-                
+
+    def inverse_kinematics(self, pose):
+        if self.ik:
+            return self.ik(pose)
+
 
 if __name__ == "__main__":
     M0 = np.array([[1, 0,  0, 0],
-                 [ 0, 1,  0, 0],
-                 [ 0, 0, 1, 0],
-                 [ 0, 0,  0, 1]])
+                   [0, 1,  0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0,  0, 1]])
 
     M1 = np.array([[1, 0,  0, 0.5],
-                 [ 0, 1,  0, 0],
-                 [ 0, 0, 1, 0],
-                 [ 0, 0,  0, 1]])
+                   [0, 1,  0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0,  0, 1]])
     M2 = np.array([[1, 0,  0, 1.5],
-                 [ 0, 1,  0, 0],
-                 [ 0, 0, 1, 0],
-                 [ 0, 0,  0, 1]])
+                   [0, 1,  0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0,  0, 1]])
     M3 = np.array([[1, 0,  0, 2.5],
-                 [ 0, 1,  0, 0],
-                 [ 0, 0, 1, 0],
-                 [ 0, 0,  0, 1]])
+                   [0, 1,  0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0,  0, 1]])
 
     M4 = np.array([[1, 0,  0, 3.0],
-                 [ 0, 1,  0, 0],
-                 [ 0, 0, 1, 0],
-                 [ 0, 0,  0, 1]])
+                   [0, 1,  0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0,  0, 1]])
 
     M_list = [M0, M1, M2, M3, M4]
-    
+
     S1 = np.array([0, 0, 1.0,  0, 0.0, 0])
     S2 = np.array([0, 0, 1.0,  0, -1.0, 0])
     S3 = np.array([0, 0, 1.0,  0, -2.0, 0])
@@ -68,5 +76,4 @@ if __name__ == "__main__":
     theta_list = [0, pi / 2, pi / 2]
 
     r = Robot(M_list, screw_list, Frame.SPACE_FRAME)
-
-    T = r.forward_kinematics(theta_list)
+    print(r.forward_kinematics([0.1, 0.2, 0.3]))
